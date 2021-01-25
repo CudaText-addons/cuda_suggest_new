7f639dfe318054e5ee8b509337c8c1348d6c5bec
Alexey <support@uvviewsoft.com>
2021-01-25 23:24:26 +0300

    fix

--- __init__.py
+++ __init__.py
@@ -7,11 +7,8 @@ class Command:
     
     def on_save_naming(self, ed_self):
         
-        suggest = ''
         cnt = ed_self.get_line_count()
         for i in range(min(MAX_LINES, cnt)):
             line = ed_self.get_text_line(i).strip()
-            print('line', line)
             if line:
-                suggest = line.replace(' ', '_').replace('\t', '_')
-                return suggest
+                return line.replace(' ', '_').replace('\t', '_')
