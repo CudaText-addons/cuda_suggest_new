96835a596a092e9105bba3c8d26d1ef8e42ed35a
Alexey <support@uvviewsoft.com>
2022-12-10 17:52:44 +0300

    on_open

--- __init__.py
+++ __init__.py
@@ -27,3 +27,7 @@ class Command:
 
         if not ed_self.get_filename():
             ed_self.set_prop(PROP_TAB_TITLE, self.get_title(ed_self))
+
+    def on_open(self, ed_self):
+
+        self.on_change_slow(ed_self)
