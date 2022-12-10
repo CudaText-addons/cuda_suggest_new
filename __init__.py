ae11afa6ce5f34ced509436b434e1991980490f3
Alexey <support@uvviewsoft.com>
2022-12-10 17:36:55 +0300

    on_change_slow

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+from cudatext import *
+
 MAX_LINES = 10
 BADS = ' \t?\'"/\\`*<>'
 
@@ -10,9 +12,18 @@ def mask_chars(s, chars):
 class Command:
     
     def on_save_naming(self, ed_self):
-        
+
+        return self.get_title(ed_self)
+
+    def get_title(self, ed_self):
+
         cnt = ed_self.get_line_count()
         for i in range(min(MAX_LINES, cnt)):
             line = ed_self.get_text_line(i).strip()
             if line:
                 return mask_chars(line, BADS)
+
+    def on_change_slow(self, ed_self):
+
+        if not ed_self.get_filename():
+            ed_self.set_prop(PROP_TAB_TITLE, self.get_title(ed_self))
